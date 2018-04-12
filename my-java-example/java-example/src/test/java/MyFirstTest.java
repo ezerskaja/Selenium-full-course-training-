52dbe3fd75965b560a59256efe769e799bc6abae
Olga Ezerskaja <o.ezerskaja@gmail.com>
2018-04-12 22:02:48 +0300

    MyFirstTest
    Test which launches some page in Chrome browser  and after that closes  the browser

---
+++ my-java-example/java-example/src/test/java/MyFirstTest.java
@@ -0,0 +1,26 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstTest {
+
+private WebDriver driver;
+
+@Before
+    public void start (){
+    driver = new ChromeDriver();
+}
+
+@Test
+    public void myFirstTest(){
+    driver.get("http://newbor.by/");
+}
+
+@After
+    public void stop(){
+    driver.quit();
+    driver = null;
+}
+}
